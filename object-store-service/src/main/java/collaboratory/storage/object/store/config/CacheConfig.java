3bbbd721343fd8dca4146690398a582edfd84505
Bob Tiernay <btiernay@oicr.on.ca>
2015-09-01 16:42:08 -0400

    Disabled caching until could be tested.

--- object-store-service/src/main/java/collaboratory/storage/object/store/config/CacheConfig.java
+++ object-store-service/src/main/java/collaboratory/storage/object/store/config/CacheConfig.java
@@ -24,22 +24,19 @@ import java.util.concurrent.ConcurrentMap;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 import com.google.common.cache.CacheBuilder;
 
 /**
  * Server wide caching configuration.
  */
-@Lazy
 @Configuration
-@EnableCaching
+// @EnableCaching
 public class CacheConfig extends CachingConfigurerSupport {
 
   /**
