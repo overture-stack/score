00d5712b63a46926b2a633435537aae29e62f590
khartmann <kevin.hartmann@oicr.on.ca>
2019-06-21 17:06:06 -0400

    Bugfix: The status bar now records writes properly for downloads

---
+++ score-client/src/main/java/bio/overture/score/client/progress/ProgressMemoryMappedInputStream.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2016 The Ontario Institute for Cancer Research. All rights reserved.                             
+ *                                                                                                               
+ * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+ * You should have received a copy of the GNU General Public License along with                                  
+ * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
+ *                                                                                                               
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package bio.overture.score.client.progress;
+
+import bio.overture.score.core.util.ForwardingInputStream;
+import bio.overture.score.core.util.ForwardingOutputStream;
+import lombok.NonNull;
+import lombok.val;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/***
+ * A variant of a ProgressInputStream for use with a ProgressMemoryMappedDataChannel
+ */
+public class ProgressMemoryMappedInputStream extends ForwardingInputStream {
+
+  private final Progress progress;
+
+  public ProgressMemoryMappedInputStream(@NonNull InputStream inputStream, @NonNull Progress progress) {
+    super(inputStream);
+    this.progress = progress;
+  }
+
+  @Override
+  public int read() throws IOException {
+    val value = super.read();
+    if (value > 0) {
+      progress.incrementBytesRead(1);
+      progress.incrementBytesWritten(1);
+    }
+
+    return value;
+  }
+
+  @Override
+  public int read(byte[] b) throws IOException {
+    val value = super.read(b);
+    if (value > 0) {
+      progress.incrementBytesRead(value);
+      progress.incrementBytesWritten(value);
+    }
+
+    return value;
+  }
+
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    val value = super.read(b, off, len);
+    if (value > 0) {
+      progress.incrementBytesRead(value);
+      progress.incrementBytesWritten(value);
+    }
+
+    return value;
+  }
+
+  @Override
+  public int hashCode() {
+    return super.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
+
+}
