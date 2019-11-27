30989eef0c1034b71dbe5ea41303b4a2b87674f1
khartmann <kevin.hartmann@oicr.on.ca>
2019-11-27 13:47:26 -0500

    Enhancement: Score client can now download analyses by specifying --project-id and --analysis-id.

---
+++ docker/score-client-init/download-analysis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+token=${1:-f69b726d-d40f-4261-b105-1ec7e6bf04d5}
+study-id=${2:-TEST-CA}
+analysis-id=${3:-f69b726d-d40f-4261-b105-1ec7e6bf04d5}
+SCORE_CLIENT="/score-client/bin/score-client"
+DIR="/tmp/download/$analysis-id"
+test -d $DIR && rm -r $DIR;mkdir -p $DIR 
+export ACCESSTOKEN=$token
+set -x
+$SCORE_CLIENT download --study-id $2 --analysis-id $3 --output-dir $DIR
