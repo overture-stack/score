07555b0f6bccf4dc5138b45faa375972df4f22e6
dahiyaAD <adahiya@oicr.on.ca>
2023-06-02 13:09:28 -0400

    CORS configurations for score server allowing multiple origins at once

--- score-server/src/main/java/bio/overture/score/server/ServerMain.java
+++ score-server/src/main/java/bio/overture/score/server/ServerMain.java
@@ -1,5 +1,6 @@
 package bio.overture.score.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Application entry point.
@@ -20,8 +24,23 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 })
 public class ServerMain {
 
+  @Value("${management.endpoints.web.cors.allowedOrigins}")
+  private String[] allowedOrigins;
+
   public static void main(String... args) {
     SpringApplication.run(ServerMain.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+          registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("PUT", "DELETE", "GET", "POST");
+      }
+    };
   }
 
 }
