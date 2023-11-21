616da15257f425e556dc3210ccd5eb6664c0cdc8
UmmulkiramR <urangwala@oicr.on.ca>
2023-11-20 20:35:40 -0500

    added a test profile

--- score-server/src/main/java/bio/overture/score/server/config/ProfileConfig.java
+++ score-server/src/main/java/bio/overture/score/server/config/ProfileConfig.java
@@ -15,7 +15,7 @@ public class ProfileConfig {
 
   @Bean
   String activeStorageProfile() {
-    Map<String, String> storageProfiles = Map.of("collaboratory", "s3", "azure", "az");
+    Map<String, String> storageProfiles = Map.of("collaboratory", "s3", "azure", "az", "test", "test");
     HashSet<String> activeStorageProfiles = new HashSet(Set.of(environment.getActiveProfiles()));
     activeStorageProfiles.retainAll(storageProfiles.keySet());
     return storageProfiles.get(activeStorageProfiles.stream().findFirst().get());
