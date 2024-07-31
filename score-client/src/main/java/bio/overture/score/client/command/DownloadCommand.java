4d3145b6e2faec2ee45f67339df955ab7dee5761
Azher2Ali <121898125+Azher2Ali@users.noreply.github.com>
2024-07-31 12:52:34 -0400

    Updating the error message

--- score-client/src/main/java/bio/overture/score/client/command/DownloadCommand.java
+++ score-client/src/main/java/bio/overture/score/client/command/DownloadCommand.java
@@ -167,7 +167,7 @@ public class DownloadCommand extends RepositoryAccessCommand {
     if (manifestResource.isGnosManifest()) {
       throw new BadManifestException(
           format(
-              "Manifest '%s' looks like a GNOS-format manifest file. Please ensure you are using a tab-delimited text file",
+              "Manifest '%s' looks like a GNOS-format manifest file. Please ensure you are using a tab-delimited text file manifest",
               manifestResource.getValue()));
     }
     val manifest = manifestService.getDownloadManifest(manifestResource);
